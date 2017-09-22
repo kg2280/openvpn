92e07137a15312f3af2f4ae7d8f8facba5c47646
Kevin Gagne <kevgagne@gmail.com>
2017-09-22 12:30:25 -0400

    Fix server certificate path and create client directory

--- create_vpn_server.sh
+++ create_vpn_server.sh
@@ -29,7 +29,7 @@ then
   echo "OpenVPN already installed"
 else
   echo "Installing OpenVPN"
-  apt-get update && apt-get dist-upgrade -y && install openvpn easy-rsa -y
+  apt-get update && apt-get dist-upgrade -y && install openvpn easy-rsa ntp ntpdate -y
 fi
 
 ## Create vpn folder and download openvpn_server.conf.
@@ -54,8 +54,8 @@ sed -i 's/export KEY_EMAIL=.*/export KEY_EMAIL="noc@cdmsfirst.com"/g' /etc/openv
 
 ## Setup key, cert and dh path in server.conf
 sed -i "s|ca /etc/openvpn/mastervpn/easy-rsa/keys/ca.crt|ca /etc/openvpn/$VPN_NAME/easy-rsa/keys/ca.crt|g" /etc/openvpn/$VPN_NAME.conf
-sed -i "s|cert /etc/openvpn/mastervpn/easy-rsa/keys/vpn.crt|cert /etc/openvpn/$VPN_NAME/easy-rsa/keys/vpn.crt|g" /etc/openvpn/$VPN_NAME.conf
-sed -i "s|key /etc/openvpn/mastervpn/easy-rsa/keys/vpn.key|key /etc/openvpn/mastervpn/easy-rsa/keys/vpn.key|g" /etc/openvpn/$VPN_NAME.conf
+sed -i "s|cert /etc/openvpn/mastervpn/easy-rsa/keys/vpn.crt|cert /etc/openvpn/$VPN_NAME/easy-rsa/keys/server.crt|g" /etc/openvpn/$VPN_NAME.conf
+sed -i "s|key /etc/openvpn/mastervpn/easy-rsa/keys/vpn.key|key /etc/openvpn/mastervpn/easy-rsa/keys/server.key|g" /etc/openvpn/$VPN_NAME.conf
 sed -i "s|dh /etc/openvpn/mastervpn/easy-rsa/keys/dh4096.pem|dh /etc/openvpn/$VPN_NAME/easy-rsa/keys/dh4096.pem|g" /etc/openvpn/$VPN_NAME.conf
 sed -i "s|tls-auth /etc/openvpn/mastervpn/easy-rsa/keys/ta.key|tls-auth /etc/openvpn/$VPN_NAME/easy-rsa/keys/ta.key|g" /etc/openvpn/$VPN_NAME.conf
 
@@ -79,6 +79,9 @@ openvpn --genkey --secret /etc/openvpn/$VPN_NAME/easy-rsa/keys/ta.key
 
 ## Create openvpn user so that openvpn does not run under root nor nobody
 adduser --system --shell /usr/sbin/nologin --no-create-home openvpn_server
+
+## Create client directory
+mkdir -p /etc/openvpn/ccd-datacenter
 
 ## Restart openvpn service
 service openvpn restart
